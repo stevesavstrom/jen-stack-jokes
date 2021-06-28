d0f0334be8ef86e3b24386c86270c86cb578eb0e
Steve Savstrom <steve.savstrom@gmail.com>
2021-06-28 13:24:33 -0500

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,42 +1,53 @@
-const express = require( 'express' );
+// creating express app...
+
+// required express - give us function
+const express = require('express');
+
+// create an instance of express by calling a function
 const app = express();
-const bodyParser = require( 'body-parser' );
-const PORT = 5000;
+const port = 5000;
 
-// use bodyParser.urlencoded throughout the app with this:
-app.use(bodyParser.urlencoded({ extended: true }));
-
-let jokes = [
-  {
-    whoseJoke: "Danny",
-    jokeQuestion: "Why do scuba divers fall backwards out of boats?",
-    punchLine: "If they fell forwards they’d still be in the boat!"
-  },
-  {
-    whoseJoke: "Luke",
-    jokeQuestion: "Two fish are in a tank. What did one fish say to the other?",
-    punchLine: "Do you know how to drive this thing?"
-  },
-  {
-    whoseJoke: "Millie",
-    jokeQuestion: "What do you call a pile of cats?",
-    punchLine: "A meowntain!"
-  },
-  {
-    whoseJoke: "dEv",
-    jokeQuestion: "Why should you not play cards in the forest?",
-    punchLine: "Too many Cheetahs!"
-  },
-  {
-    whoseJoke: "Scott",
-    jokeQuestion: "I went to the zoo the other day, it had one dog...",
-    punchLine: "It was a shih tzu."
-  }
-];
-
-// serve back static files
+// express static file serving - public is folder name:
 app.use(express.static('server/public'));
 
-app.listen(PORT, () => {
-  console.log('server running on: ', PORT);
-}); // end spin up server
+// app.use(bodyparser.urlencoded({extended: true})); // older
+app.use(express.urlencoded({ extended: true }));
+
+// JSON function
+app.use(express.json());
+
+// start up  server
+app.listen(port, () => {
+	console.log('listening on port', port);
+  });
+
+  let jokes = [
+	{
+	  whoseJoke: "Danny",
+	  jokeQuestion: "Why do scuba divers fall backwards out of boats?",
+	  punchLine: "If they fell forwards they’d still be in the boat!"
+	},
+	{
+	  whoseJoke: "Luke",
+	  jokeQuestion: "Two fish are in a tank. What did one fish say to the other?",
+	  punchLine: "Do you know how to drive this thing?"
+	},
+	{
+	  whoseJoke: "Millie",
+	  jokeQuestion: "What do you call a pile of cats?",
+	  punchLine: "A meowntain!"
+	},
+	{
+	  whoseJoke: "dEv",
+	  jokeQuestion: "Why should you not play cards in the forest?",
+	  punchLine: "Too many Cheetahs!"
+	},
+	{
+	  whoseJoke: "Scott",
+	  jokeQuestion: "I went to the zoo the other day, it had one dog...",
+	  punchLine: "It was a shih tzu."
+	}
+  ];
+  
+
+
